c8dbcec68dd67c357e9d8f52a3bfa9b254b40b3e
youchao liu <ryuforever@163.com>
2020-11-21 18:54:39 +0800

    fix use proxy

--- src/commons.ts
+++ src/commons.ts
@@ -30,7 +30,7 @@ export const canIUseProxy = () => {
     return false;
   }
 
-  return false;
+  return true;
 };
 
 export const hasOwnProperty = (o: object, prop: PropertyKey) => o.hasOwnProperty(prop) // eslint-disable-line
